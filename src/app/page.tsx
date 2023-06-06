d8fe6f9a2e56a8b452317773cca67b692fb2d1c8
James Redding <jdr161@case.edu>
2023-06-06 15:03:10 -0500

    added button to make API call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+'use client';
 import Image from 'next/image'
+import { useState } from 'react';
 
 export default function Home() {
+  const axios = require('axios');
+  const [ text, setText ] = useState("output");
+  const callAPI = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setText(JSON.stringify(response));
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -28,15 +42,15 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="relative flex place-items-center">
+        <ul>
+          <li>
+            <button onClick={callAPI}>Make API Call</button>
+          </li>
+          <li>
+            <p>{text}</p>
+          </li>
+        </ul>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
